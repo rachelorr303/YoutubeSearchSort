69456192e4530a430a947fccc8b1004198969bc2
Rachel Orr <78055465+rachelorr303@users.noreply.github.com>
2021-04-11 16:33:23 -0400

    added input options to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,12 @@ using namespace std;
 int main()
 {   
     cout << "Hello World!" << endl;
+    cout << "Welcome to the Viral Video-inator!" << endl;
+    cout << "Please select one of the following options: " << endl;
+    cout << "1. Channel Search" << endl << "2. Video Search" << endl << "3. Most popular publishing days of the week" << endl;
+    cout << "4. Most popular publishing time frame" << endl << "Exit" << endl;
+    
+    
+    
     return 0;
 }
