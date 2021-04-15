035a6952e451ca6176c8b53d611f2d6e4176a57b
Monique Huang <39775316+moniquehuang@users.noreply.github.com>
2021-04-15 16:39:06 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,45 +1,95 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-int main()
-{   
-    string userInput;
-    
-    cout << "Hello World!" << endl;
-    cout << "Welcome to the Viral Video-inator!" << endl;
-    cout << "Please select one of the following options: " << endl;
-    cout << "1. Channel Search" << endl << "2. Video Search" << endl << "3. Most popular publishing days of the week" << endl;
-    cout << "4. Most popular publishing time frame" << endl << "Exit" << endl;
-    
-    cin >> userInput;
-    
-    while(userInput != "Exit"){
-    
-        if(userInput == "1"){
-            //channel search
-            
+class Node {
+public:
+    string trending;
+    string title;
+    string channel;
+    string published;
+    int time;
+    string day;
+    int tags;
+    int views;
+    int likes;
+    int dislikes;
+    int comments;
+    Node *left;
+    Node *right;
+    Node();
+    Node(string trend, string tile, string chan, string pub, int tm, string da, int tgs, int v, int ls, int dls, int cc) : trending(trend), title(tile), channel(chan), published(pub), time(tm), day(da), tags(tgs), views(v), likes(ls), dislikes(dls), comments(cc) {}
+};
+
+Node::Node() {
+    trending = "";
+    title = "";
+    channel = "";
+    published = "";
+    time = 0;
+    day = "";
+    tags = 0;
+    views = 0;
+    likes = 0;
+    dislikes = 0;
+    comments = 0;
+    left = nullptr;
+    right = nullptr;
+}
+
+class BST {
+public:
+    BST();
+    Node *root;
+    Node* insert(string trend, string title, string chan, string pub, int time, string day, int tags, int v, int ls, int dls, int cc, Node* curr);
+    Node* searchTitle(string title, int v, Node*);
+    void searchChannel(string chan, Node*);
+};
+
+BST::BST() {
+    root = nullptr;
+}
+
+Node *
+BST::insert(string trend, string title, string chan, string pub, int time, string day, int tags, int v, int ls, int dls,
+            int cc, Node* curr) {
+    if(searchTitle(title, v, curr) == nullptr) {
+        if(curr == nullptr) { //current node is empty or its root of tree
+            curr = new Node(trend, title, chan, pub, time, day, tags, v, ls, dls, cc);
+            return curr;
         }
-        else if(userInput == "2"){
-            //video search
-            
+        else if(curr->views > v){ //store new node in left subtree
+            curr->left = insert(trend, title, chan, pub, time, day, tags, v, ls, dls, cc, curr->left);
         }
-        else if(userInput == "3"){
-            //most popular publishing days
-            
+        else if(curr->views < v){ //store new node in right subtree
+            curr->right = insert(trend, title, chan, pub, time, day, tags, v, ls, dls, cc, curr->right);
         }
-        else if(userInput == "4"){
-            //most popular publishing time frame
-            
-        }
-        else if(userInput == "Exit"){
-            cout << "Exiting program" << endl;
-        }
-        else{
-            cout << "Please enter appropriate input in the form of an integer" << endl;
-        }
-   }
-    
+    }
+    return curr;
+}
+
+Node* BST::searchTitle(string title, int views, Node *curr) {
+    Node* temp = new Node();
+    if (curr == nullptr) {
+        cout << "Title not found." << endl;
+        return nullptr;
+    }
+    else if(curr->title==title) { //if current node's name is equal to searched name
+        temp = curr;
+    }
+    else if(curr->views > views) {
+        return searchTitle(title, views, curr->left);
+    }
+    else {
+        return searchTitle(title, views, curr->right);
+    }
+    return temp;
+}
+
+void BST::searchChannel(string chan, Node *) {
+    return;
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
