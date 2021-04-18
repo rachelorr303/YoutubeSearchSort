35e97fbf8a02ffb6a4f634c35567194318e6c48d
Rachel Orr <78055465+rachelorr303@users.noreply.github.com>
2021-04-18 15:47:38 -0400

    Added loading data to main.cpp
    the data is loaded just before the user is prompted to input their requests. As of right now, everything is stored in string vectors.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,13 @@
 using namespace std;
 
 
+
 int main()
 {
-    cout << "This is the basic parsing info" << endl;
+        
+    /*LOADING DATA*/
     
-    //this is where we open the file, made sure it is in csv comma deliminated format
+     //this is where we open the file, made sure it is in csv comma deliminated format
     ifstream file;
     file.open("YouTubeData.csv", ios::in );
 
@@ -38,8 +40,6 @@ int main()
     vector<string> commentsDisabled;
     vector<string> ratingsDisabled;
     vector<string> vidErrorRemoved;
-
-
 
     if (file.is_open()) {
         cout << "opened file" << endl;
@@ -104,5 +104,40 @@ int main()
         file.close();
     }
 
+    
+    string userInput;
+
+    cout << "Hello World!" << endl;
+    cout << "Welcome to the Viral Video-inator!" << endl;
+    cout << "Please select one of the following options: " << endl;
+    cout << "1. Channel Search" << endl << "2. Video Search" << endl << "3. Most popular publishing days of the week" << endl;
+    cout << "4. Most popular publishing time frame" << endl << "Exit" << endl;
+
+    cin >> userInput;
+
+    while(userInput != "Exit"){
+
+        if(userInput == "1"){
+            //channel search
+
+        }
+        else if(userInput == "2"){
+            //video search
+
+        }
+        else if(userInput == "3"){
+            //most popular publishing days
+
+        }
+        else if(userInput == "4"){
+            //most popular publishing time frame
+
+        }
+        else if(userInput == "Exit"){
+            cout << "Exiting program" << endl;
+        }
+        else{
+            cout << "Please enter appropriate input in the form of an integer" << endl;
+        }
 
 }
