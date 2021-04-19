c1d1c1a8dc99b1f759164bafac4d3f1929fbb8e3
sofiavaldes <61989747+sofiavaldes@users.noreply.github.com>
2021-04-19 12:37:55 -0400

    Update AVL.h

--- AVL.h
+++ AVL.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include "Node.h"
 using namespace std;
 
@@ -13,9 +14,8 @@ public:
     vector<Node*> channels;
     vector<Node*> ranges;
     AvlTree();
-    Node* insertViews(string trend, string title_, string chan, string pub, string t, string d, long v, long l, long dl, int c, vector<string> tag, Node* root);
-    Node* searchTitle(string t, int v, Node* node);
-    void searchTitle2(string t, Node* node);
+    Node* insertViews(string trend, string title_, string chan, string pub, string t, string d, long v, long l, long dl, int c, Node* root);
+    void searchTitle(string t, Node* node, bool& found);
     void searchWord(string key, Node* node);
     void searchChannel(string chan, Node* node);
     void searchViews(int max, int min, Node* node);
@@ -24,4 +24,5 @@ public:
     Node* leftRotation(Node* node);
     Node* rightRotation(Node* node);
     void printInfo(Node* node);
+    void getStats(vector<Node*> group);
 };
